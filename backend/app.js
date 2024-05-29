325242e59070b92ecbaf5913b3ae925d76b8b4d1
Guillaume Destree <devstree.guillaume@gmail.com>
2024-05-29 16:32:07 +0200

    Maj esLint/ ajout de mongodb

--- backend/app.js
+++ backend/app.js
@@ -1,23 +1,16 @@
-const express = require("express");
+const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express();
+mongoose.connect('mongodb+srv://azerty-uiop:123456lkl@cluster0.sqhkj3s.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
 
-app.use((req, res, next) => {
-  console.log("Requête reçue !");
-  next();
-});
+app.use((req,res)=>{
+    res.json({message:'Votre requête à bien été recue !'})
+})
 
-app.use((req, res, next) => {
-  res.status(201);
-  next();
-});
-
-app.use((req, res, next) => {
-  res.json({ message: "Votre requête a bien été reçue !" });
-  next();
-});
-
-app.use((req, res, next) => {
-  console.log("Réponse envoyée avec succès !");
-});
 
 module.exports = app;
