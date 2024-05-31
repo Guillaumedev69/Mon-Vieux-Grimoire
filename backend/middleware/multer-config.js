662afa9539c06ab1f209b07fd0e2d1df491dfff2
Guillaume Destree <devstree.guillaume@gmail.com>
2024-05-31 12:37:51 +0200

    remplacement image à la modif ok pour DB mais pas de suppression dans le dossier images et voir qualité image

--- backend/middleware/multer-config.js
+++ backend/middleware/multer-config.js
@@ -1,4 +1,7 @@
 const multer = require("multer");
+const sharp = require("sharp");
+const path = require("path");
+const fs = require("fs");
 
 const MIME_TYPES = {
   "image/jpg": "jpg",
@@ -6,8 +9,11 @@ const MIME_TYPES = {
   "image/png": "png",
 };
 
+const widthInPixels = Math.round(12.87 * 16);
+const heightInPixels = Math.round(16.27 * 16);
+
 const storage = multer.diskStorage({
-  destination: (req, fil, callback) => {
+  destination: (req, file, callback) => {
     callback(null, "images");
   },
   filename: (req, file, callback) => {
@@ -17,4 +23,50 @@ const storage = multer.diskStorage({
   },
 });
 
-module.exports = multer({ storage }).single("image");
+const upload = multer({ storage }).single("image");
+
+const deleteImage = (filePath) => {
+  fs.unlink(filePath, (err) => {
+    if (err) {
+      console.error(`Failed to delete image at ${filePath}:`, err.message);
+    }
+  });
+};
+
+module.exports = (req, res, next) => {
+  upload(req, res, (err) => {
+    if (err) {
+      return res.status(500).json({ error: err.message });
+    }
+
+    if (!req.file) {
+      return res.status(400).json({ error: "No file uploaded" });
+    }
+
+    const filePath = path.join("images", req.file.filename);
+    const outputFilePath = path.join("images", "resized-" + req.file.filename);
+
+    sharp(filePath)
+      .resize(widthInPixels, heightInPixels)
+      .toFormat(req.file.mimetype.split('/')[1], {
+        quality: 90,
+        progressive: true,
+        compressionLevel: 9,
+      })
+      .toFile(outputFilePath, (err, info) => {
+        if (err) {
+          return res.status(500).json({ error: err.message });
+        }
+
+        // Delete the original file after the resized file is created
+        setTimeout(() => deleteImage(filePath), 1000);
+
+        // Attach the resized image path to the request object
+        req.file.path = outputFilePath;
+        req.file.filename = "resized-" + req.file.filename;
+        next();
+      });
+  });
+};
+
+module.exports.deleteImage = deleteImage;
--- backend/routes/books.js
+++ backend/routes/books.js
@@ -7,9 +7,12 @@ const multer = require("../middleware/multer-config");
 router.get("/", booksCtrl.getAllBook);
 router.get("/bestrating", booksCtrl.getBestRating);
 router.get("/:id", booksCtrl.getOneBook);
-router.post("/", auth, multer, booksCtrl.createBook);
+
+router.post("/", auth, multer,  booksCtrl.createBook);
 router.post("/:id/rating", auth, booksCtrl.createRating);
-router.put("/:id", auth, multer, booksCtrl.modifyBook);
+
+router.put("/:id", auth, multer,  booksCtrl.modifyBook);
+
 router.delete("/:id", auth, booksCtrl.deleteBook);
 
 module.exports = router;
